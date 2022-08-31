ba2e90120da588e8e50155c8ab4321c06192c8dc
dorimusz <dorottya.kocsis.it@gmail.com>
2022-08-31 15:11:53 +0200

    custom link hook

--- academind-cc/pages/news/index.js
+++ academind-cc/pages/news/index.js
@@ -1,7 +1,22 @@
 // my-domain.com/news
 
+import { Fragment } from "react";
+import Link from "next/link"; //to stay single paged  in order to preserve states
+
 function NewsPage() {
-  return <h1>Hello girl, news</h1>;
+  return (
+    <Fragment>
+      <h1>Hello girl, news</h1>
+      <ul>
+        <li>
+          <Link href="/news">Its great</Link>
+        </li>
+        <li>
+          <Link href="/news/wunderbar">Wunderbar</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
